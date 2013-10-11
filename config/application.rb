4e8312a6456f920b15e803fc28cbc15a7d171ebf
RyanAdmin <ryandeford@gmail.com>
2013-10-10 22:49:37 -0400

    Setting up Bootstrap 3

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module RyandefordCom
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add fonts for bootstrap to asset paths
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
